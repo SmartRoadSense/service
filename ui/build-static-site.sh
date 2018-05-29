a99a43333a84fdd8b196cfba0517cc34c6e711d7
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-05-29 21:45:14 +0200

    Avoid full website removal when rebuilding

--- ui/build-static-site.sh
+++ ui/build-static-site.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Clean-up output directory
-rm -rf /output/*
-
 # Get website source
 git clone https://github.com/SmartRoadSense/website.git /tmp/repository
 mv /tmp/repository/hugo/* /src
