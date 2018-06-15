85cf971f5dbd4f51cd553a146cb925771540e64e
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-06-15 17:05:39 +0200

    Replace Node.JS tile server with blank ASP.NET Core server

---
+++ tiles/TileServer/Controllers/TileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TileServer.Controllers {
+
+    [Route("api/srs-tiles/")]
+    public class TileController : Controller {
+
+        [HttpGet("{zoom}/{x}/{y}")]
+        public ActionResult Get(int zoom, double x, double y) {
+            Debug.WriteLine($"Tile {x},{y} zoom {zoom}");
+
+            return null;
+        }
+
+    }
+
+}
