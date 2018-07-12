47443e30d0cecd0c33384baafa9210db8fb5e600
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-07-12 16:57:46 +0200

    Switch to Debian-based UI image for Hugo 0.43
    New Hugo 0.43 with SASS/minification support depends on libraries not available on Alpine. This switches the UI image to a heavy-weight Debian base image.
    As soon as this issue is solved, we should switch back to klakegg/hugo or jojomi/hugo.

--- ui/build-static-site.sh
+++ ui/build-static-site.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Get website source
+echo "Retrieving website source…"
 git clone https://github.com/SmartRoadSense/website.git /tmp/repository
 mv /tmp/repository/hugo/* /src
 
-# Go on with default Hugo build
-/run.sh
+echo "Building website…"
+hugo --source="/src" --destination="/output" --baseURL="$HUGO_BASEURL" "$@" || exit 1
