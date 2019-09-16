474e37ea1f413596b7bc55f1274617857f0374d4
Saverio Delpriori <saverio.delpriori@uniurb.it>
2019-09-16 08:38:46 -0400

    Remove unused containers and code

--- agg/cli/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#psql -c 'CREATE USER srs_ro_user;'
-psql -f /code/tables.sql
-psql -f /code/functions.sql
--- raw/cli/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#psql -c 'CREATE USER srs_ro_user;'
-#psql -c 'CREATE EXTENSION postgis;'
-#psql -c 'CREATE EXTENSION postgis_topology;'
-psql -f /code/tables.sql
-psql -f /code/functions.sql
