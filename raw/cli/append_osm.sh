b9730ae5d1b5da2dfbd0271a5e48a722345f7486
Saverio Delpriori <saverio.delpriori@uniurb.it>
2019-12-19 04:54:02 -0500

    Bugfix 'data' volume visibility and OSM data append procedure

--- raw/cli/append_osm.sh
+++ raw/cli/append_osm.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-osm2pgsql --number-processes 8 -a -d ${PGDATABASE} -H ${PGHOST} -P ${PGPORT} -l /data/map.osm.pbf
+osm2pgsql --number-processes 8 -a -s -d ${PGDATABASE} -H ${PGHOST} -P ${PGPORT} -l /data/map.osm.pbf
